0d16a96936c83070394afcf302d2f4e71cdc119c
David <davidakovalchuk@gmail.com>
2023-02-10 16:47:52 -0800

    Connected the data and fix navbar.

--- server/Routes/general.js
+++ server/Routes/general.js
@@ -1,5 +1,8 @@
 import express from "express";
+import { getUser } from "../Controllers/general.js";
 
 const router = express.Router();
+
+router.get("/user/:id", getUser);
 
 export default router;
